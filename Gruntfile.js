10698ecbb293b034c87b081b63da554f79ad6705
Dewitt Buckingham <dj729@comcast.net>
2015-01-29 22:55:01 -0800

    Setup Workspace
    * Setup Node (package.json)
    * Change .gitignore back
    * Add Sass
    * Add Watch and LiveReload
    * Add JShint and Concat

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            compile: {
+                expand: true,
+                cwd: 'sass',
+                src: ['**/*.scss'],
+                dest: 'public',
+                ext: '.css',
+                style: 'expanded'
+            }
+        },
+        watch: {
+            options: {
+                livereload: true,
+            },
+            sass: {
+                files: 'sass/**/*.scss',
+                tasks: ['sass']
+            },
+            js: {
+                files: 'javascript/**/*.js',
+                tasks: ['scripts']
+            }
+
+        },
+        jshint: {
+            grunt: ['Gruntfile.js'],
+            before: ['javascript/**/*.js'],
+            after: ['public/script.js']
+        },
+        concat: {
+            js: {
+                src: ['javascript/**/*.js'],
+                dest: 'public/script.js'
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('scripts', ['jshint:before', 'concat', 'jshint:after']);
+    grunt.registerTask('styles', ['sass']);
+    grunt.registerTask('default', ['jshint:grunt', 'styles', 'scripts', 'watch']);
+};
