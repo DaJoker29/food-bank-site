3c201461a9a3ef52140e803ad66ec61408ab8d29
Dewitt Buckingham <dj729@comcast.net>
2015-02-06 23:23:00 -0800

    Add Top of Page Links (Closes #2)
    * Create links that go back to the top of the page
    * Add smooth scrolling effect

--- templates/donate.php
+++ templates/donate.php
@@ -1,5 +1,5 @@
 <section id="donate" class="section">
-    <h2 class="section-heading">Donate</h2>
+    <h2 class="section-heading">Donate <a data-scroll href="#" class="top">Back To Top</a></h2>
     <p class="call-to-action">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis numquam praesentium explicabo ipsum iure alias vero, obcaecati magni, sit nobis soluta iusto, facilis aspernatur tenetur consequuntur porro voluptate tempora beatae?</p>
     <a href="https://mkt.com/zero-daedalus" class="sq-embed-button" data-style-background-color="#333333" data-style-width="10rem" data-style-height="46px">Donate</a>
     <script src="https://cdn.sq-api.com/market/embed.js" charset="utf-8"></script>
--- templates/staff.php
+++ templates/staff.php
@@ -1,5 +1,5 @@
 <section id="staff" class="section">
-    <h2 class="section-heading">Staff</h2>
+    <h2 class="section-heading">Staff <a data-scroll href="#" class="top">Back To Top</a></h2>
     <img src="http://placehold.it/700x350">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus est fugiat necessitatibus aperiam, quo accusantium maxime iste unde dolorum placeat iure corrupti corporis optio quam dolorem blanditiis, vero fugit illum!</p>
 </section>
