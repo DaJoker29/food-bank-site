3a6d1864216ae054ae11ef349a1bdc4790200aaa
Dewitt Buckingham <dj729@comcast.net>
2015-02-10 23:36:23 -0800

    Adjust BTT Links
    Back to Top links only hide/show on desktops

--- templates/staff.php
+++ templates/staff.php
@@ -1,5 +1,6 @@
 <section id="staff" class="section">
-    <h2 class="section-heading">Staff <a data-scroll href="#" class="top">Back To Top</a></h2>
+    <h2 class="section-heading">Staff</h2>
+    <a data-scroll href="#" class="top">Back To Top</a>
     <img src="http://placehold.it/700x350">
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus est fugiat necessitatibus aperiam, quo accusantium maxime iste unde dolorum placeat iure corrupti corporis optio quam dolorem blanditiis, vero fugit illum!</p>
 </section>
