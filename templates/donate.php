bc5c75d7cc5eb9ebdf09bbb8ac8eb8034331537b
Dewitt Buckingham <dj729@comcast.net>
2015-02-12 21:55:18 -0800

    Design Donation Interface
    * Build non-interactive donation mockup

--- templates/donate.php
+++ templates/donate.php
@@ -1,7 +1,17 @@
 <section id="donate" class="section">
     <h2 class="section-heading">Donate</h2>
     <a data-scroll href="#" class="top">Back To Top</a>
-    <p class="call-to-action">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis numquam praesentium explicabo ipsum iure alias vero, obcaecati magni, sit nobis soluta iusto, facilis aspernatur tenetur consequuntur porro voluptate tempora beatae?</p>
-    <a href="https://mkt.com/zero-daedalus" class="sq-embed-button" data-style-background-color="#333333" data-style-width="10rem" data-style-height="46px">Donate</a>
-    <script src="https://cdn.sq-api.com/market/embed.js" charset="utf-8"></script>
+    <form action="" class="donate-form" name="donate">
+        <div class="info-group">
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Organization">
+            <input type="email" placeholder="Email Address">
+            <input type="text" placeholder="Amount (PayPal only)">
+        </div>
+
+        <div class="btn-group">
+            <input type="button" value="Donate via PayPal">
+            <input type="button" value="Donate via Square">
+        </div>
+    </form>
 </section>
