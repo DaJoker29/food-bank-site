cf1c912b117a11ac1695e25f27b3215ede5dc330
Dewitt Buckingham <dj729@comcast.net>
2015-02-13 17:19:53 -0800

    Add letter-spacing
    * Add spacing to display font elements
    * Add spacing to nav list elements
    * Adjust breakpoints to compensate for changes

--- templates/heading.php
+++ templates/heading.php
@@ -2,7 +2,7 @@
 <div class="masthead">
     <header class="site-heading">
         <h1 class="site-title__small">Interfaith Food Bank</h1>
-        <h1 class="site-title__def">IFB</h1>
+        <h1 class="site-title__def">Interfaith</h1>
         <nav class="site-nav">
             <ul class="nav-list">
                 <li class="nav-item"><a data-scroll href="#info">Info</a></li>
