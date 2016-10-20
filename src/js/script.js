f9cafd65365a1dd3c23829d61195bc09a2986c94
Dewitt Buckingham <dj729@comcast.net>
2016-10-19 18:54:01 -0700

    Contact Modal
    Added a contact form modal. Also tweaked the about button CTA to toggle
    this one on and that one off.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
 $('#aboutModal').on('hide.bs.modal', function(e) {
   $('video').trigger('pause');
 });
+
+$('#aboutModalCTA').on('click', function(e) {
+  $('#contactModal').modal('show');
+  $('#aboutModal').on('hidden.bs.modal', function(e) {
+    $('body').addClass('modal-open');
+    $('#aboutModal').off('hidden.bs.modal');
+  });
+});
