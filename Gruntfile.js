2a26372f95432d5f6abf0bcdfe544cccfa79ab70
Dewitt Buckingham <dj729@comcast.net>
2016-10-26 16:39:29 -0700

    # This is a combination of 4 commits. # The first commit's message is: Package Management
    Making it sexy.
    
    # This is the 2nd commit message:
    
    Sass Config
    
    # This is the 3rd commit message:
    
    Added More Dir Structure
    
    Made js and css directories to help organize more code.
    
    # This is the 4th commit message:
    
    Remove Yarn
    
    NO need to experiment just yet.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,82 +1,134 @@
+/* eslint-disable */
 module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        watch: {
-            sass: {
-                files: ['src/scss/**/*.scss'],
-                tasks: ['sass', 'postcss']
-            },
-            js: {
-                files: ['src/js/**/*.js'],
-                tasks: ['eslint', 'uglify']
-            },
-            livereload: {
-                options: { livereload: true },
-                files: ['dist/**/*']
-            }
-        },
-        sass: {
-            dev: {
-                expand: true,
-                cwd: 'src/scss',
-                src: ['**/*.scss'],
-                dest: 'dist',
-                ext: '.css',
-                options: {
-                    style: 'expanded',
-                    sourcemap: 'none'
-                }
-            },
-            prod: {
-               expand: true,
-                cwd: 'src/scss',
-                src: ['**/*.scss'],
-                dest: 'dist',
-                ext: '.min.css',
-                options: {
-                    style: 'compressed'
-                }
-            }
-        },
-        postcss: {
-            options: {
-                processors: [
-                    require('autoprefixer-core')({browsers: 'last 2 versions'})
-                ]
-            },
-            dev: {
-                src: 'dist/style.css',
-                map: true
-            },
-            prod: {
-                src: 'dist/style.min.css'
-            }
-        },
-        uglify: {
-            dev: {
-                options: {
-                    sourceMap: true,
-                    mangle: false,
-                    beautify: true,
-                    preserveComments: 'all',
-                    compress: false
-                },
-                files: {
-                    'dist/script.js': ['src/js/**/*.js']
-                }
-            },
-            prod: {
-                files: {
-                    'dist/script.min.js': ['src/js/**/*.js']
-                }
-            }
-        },
-        eslint: {
-            target: ['src/js/**/*.js']
+  grunt.initConfig({
+    watch: {
+      sass: {
+        files: ['src/scss/**/*.scss'],
+        tasks: ['build-css']
+      },
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['build-js']
+      },
+      html: {
+        files: ['src/html/**/*.html'],
+        tasks: ['copy:html']
+      },
+      vendor: {
+        files: ['vendor/**'],
+        tasks: ['copy:vendor']
+      },
+      assets: {
+        files: ['assets/**'],
+        tasks: ['copy:assets']
+      },
+      livereload: {
+        options: { livereload: true },
+        files: ['dist/**/*']
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/css/style.css': ['src/scss/style.scss']
         }
-    });
+      }
+    },
+    postcss: {
+      options: {
+        processors: [
+        require('autoprefixer')({browsers: 'last 2 versions'})
+        ]
+      },
+      dist: {
+        src: 'dist/css/style.css',
+        map: true
+      }
+    },
+    cssmin: {
+      options: {
+        sourceMap: true
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'dist/css',
+          src: 'style.css',
+          dest: 'dist/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    uglify: {
+      dev: {
+        options: {
+          sourceMap: true,
+          mangle: false,
+          beautify: true,
+          preserveComments: 'all',
+          compress: false
+        },
+        files: {
+          'dist/js/script.js': ['src/js/**/*.js']
+        }
+      },
+      prod: {
+        options: {
+          sourceMap: true
+        },
+        files: {
+          'dist/js/script.min.js': ['src/js/**/*.js']
+        }
+      }
+    },
+    eslint: {
+      target: ['src/js/**/*.js']
+    },
+    clean: [ 'dist' ],
+    copy: {
+      vendor: {
+        expand: true,
+        src: 'vendor/**',
+        dest: 'dist/'
+      },
+      assets: {
+        expand: true,
+        src: 'assets/**',
+        dest: 'dist/'
+      },
+      html: {
+        expand: true,
+        cwd: 'src/html',
+        src: '**',
+        dest: 'dist/'
+      }
+    }
+  });
 
-    grunt.registerTask('build', 'Build project', ['eslint', 'uglify', 'sass', 'postcss'])
-    grunt.registerTask('default', 'Build development version and run watch server', ['build', 'watch']);
+  // JS Build Tasks
+  grunt.registerTask(
+    'build-js',
+    'Compile Scripts',
+    ['eslint', 'uglify']
+    );
+  grunt.registerTask(
+    'build-css',
+    'Compile CSS',
+    ['sass', 'postcss', 'cssmin']
+    );
+  grunt.registerTask(
+    'build',
+    'Build project',
+    ['clean', 'copy', 'build-js', 'build-css']
+    );
+  grunt.registerTask(
+    'default',
+    'Build development version and run watch server',
+    ['build', 'watch']
+    );
 };
