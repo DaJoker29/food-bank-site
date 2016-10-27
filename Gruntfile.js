6d6e4401250b9d7ac8d3215c34f4dfb000cc2a71
Dewitt Buckingham <dj729@comcast.net>
2016-10-26 20:07:34 -0700

    Sass Config

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,11 @@ module.exports = function(grunt) {
     watch: {
       sass: {
         files: ['src/scss/**/*.scss'],
-        tasks: ['sass', 'postcss']
+        tasks: ['build-css']
       },
       js: {
         files: ['src/js/**/*.js'],
-        tasks: ['eslint', 'uglify']
+        tasks: ['build-js']
       },
       html: {
         files: ['src/html/**/*.html'],
@@ -30,87 +30,85 @@ module.exports = function(grunt) {
       }
     },
     sass: {
-      dev: {
-        expand: true,
-        cwd: 'src/scss',
-        src: ['**/*.scss'],
-        dest: 'dist',
-        ext: '.css',
-        options: {
-          style: 'expanded',
-          sourcemap: 'none'
-        }
-      },
-      prod: {
-       expand: true,
-       cwd: 'src/scss',
-       src: ['**/*.scss'],
-       dest: 'dist',
-       ext: '.min.css',
-       options: {
-        style: 'compressed'
-      }
-    }
-  },
-  postcss: {
-    options: {
-      processors: [
-      require('autoprefixer')({browsers: 'last 2 versions'})
-      ]
-    },
-    dev: {
-      src: 'dist/style.css',
-      map: true
-    },
-    prod: {
-      src: 'dist/style.min.css'
-    }
-  },
-  uglify: {
-    dev: {
-      options: {
-        sourceMap: true,
-        mangle: false,
-        beautify: true,
-        preserveComments: 'all',
-        compress: false
-      },
-      files: {
-        'dist/script.js': ['src/js/**/*.js']
-      }
-    },
-    prod: {
       options: {
         sourceMap: true
       },
-      files: {
-        'dist/script.min.js': ['src/js/**/*.js']
+      dist: {
+        files: {
+          'dist/style.css': ['src/scss/style.scss']
+        }
+      }
+    },
+    postcss: {
+      options: {
+        processors: [
+        require('autoprefixer')({browsers: 'last 2 versions'})
+        ]
+      },
+      dist: {
+        src: 'dist/style.css',
+        map: true
+      }
+    },
+    cssmin: {
+      options: {
+        sourceMap: true
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'dist',
+          src: 'style.css',
+          dest: 'dist',
+          ext: '.min.css'
+        }]
+      }
+    },
+    uglify: {
+      dev: {
+        options: {
+          sourceMap: true,
+          mangle: false,
+          beautify: true,
+          preserveComments: 'all',
+          compress: false
+        },
+        files: {
+          'dist/script.js': ['src/js/**/*.js']
+        }
+      },
+      prod: {
+        options: {
+          sourceMap: true
+        },
+        files: {
+          'dist/script.min.js': ['src/js/**/*.js']
+        }
+      }
+    },
+    eslint: {
+      target: ['src/js/**/*.js']
+    },
+    clean: [ 'dist' ],
+    copy: {
+      vendor: {
+        expand: true,
+        src: 'vendor/**',
+        dest: 'dist/'
+      },
+      assets: {
+        expand: true,
+        src: 'assets/**',
+        dest: 'dist/'
+      },
+      html: {
+        expand: true,
+        cwd: 'src/html',
+        src: '**',
+        dest: 'dist/'
       }
     }
-  },
-  eslint: {
-    target: ['src/js/**/*.js']
-  },
-  clean: [ 'dist' ],
-  copy: {
-    vendor: {
-      expand: true,
-      src: 'vendor/**',
-      dest: 'dist/'
-    },
-    assets: {
-      expand: true,
-      src: 'assets/**',
-      dest: 'dist/'
-    },
-    html: {
-      expand: true,
-      cwd: 'src/html',
-      src: '**',
-      dest: 'dist/'
-    }
-  }
-});
+  });
 
   // JS Build Tasks
   grunt.registerTask(
@@ -118,11 +116,10 @@ module.exports = function(grunt) {
     'Compile Scripts',
     ['eslint', 'uglify']
     );
-
   grunt.registerTask(
     'build-css',
     'Compile CSS',
-    ['sass', 'postcss']
+    ['sass', 'postcss', 'cssmin']
     );
   grunt.registerTask(
     'build',
